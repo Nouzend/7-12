d4bf7e24ab34009adefe2eea5c766f06223348f0
HARU\serru <serruku@gmail.com>
2023-01-06 11:41:18 +0700

    อัปโหลดโค้ดทั้งหมด

---
+++ 7APP/SRC/config/Constant.js
@@ -0,0 +1,29 @@
+const DB_HOST = "ap-southeast.connect.psdb.cloud"
+const DB_USER = "c7x0cg6vy3ezlpug7070"
+const DB_PASSWORD = "pscale_pw_4JcWIAvJgrfO7pxoJsEEqwAkjvRdxmpWMT8LgHNwbxf"
+const DB_DATABAST = "7Online"
+const DB_PORT = 3306
+
+// sever
+const SEVER_PORT = 3000
+
+// Authentication
+const JWT_SECRET = ""
+const JWT_EXPIRE_TIMEOUT = 3600
+
+// ENum
+const ADMIN_ROLE = 'admin'
+const USER_ROLE = 'user'
+
+module.exports = {
+    DB_HOST,
+    DB_USER,
+    DB_PASSWORD,
+    DB_DATABASE,
+    DB_PORT,
+    SERVER_PORT,
+    JWT_SECRET,
+    JWT_EXPIRE_TIMEOUT,
+    ADMIN_ROLE,
+    USER_ROLE
+}
